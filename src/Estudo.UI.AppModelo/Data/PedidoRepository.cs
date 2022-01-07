0753523a0fdbbdbbb6dbaa8323df401db713d731
Karollainny Teles <karollainnyteles@hotmail.com>
2022-01-07 15:56:02 -0300

    Adicionando  a entidade Pedido, PedidoRepository e IPedidoRepository

---
+++ src/Estudo.UI.AppModelo/Data/PedidoRepository.cs
@@ -0,0 +1,17 @@
+﻿using Estudo.UI.AppModelo.Models;
+
+namespace Estudo.UI.AppModelo.Data
+{
+    public class PedidoRepository : IPedidoRepository
+    {
+        public Pedido ObterPedido()
+        {
+            return new Pedido();
+        }
+    }
+
+    public interface IPedidoRepository
+    {
+        Pedido ObterPedido();
+    }
+}
---
+++ src/Estudo.UI.AppModelo/Models/Pedido.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Estudo.UI.AppModelo.Models
+{
+    public class Pedido
+    {
+        public Guid Id { get; set; }
+
+        public Pedido()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
