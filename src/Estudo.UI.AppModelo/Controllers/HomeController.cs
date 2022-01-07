4e512a1144f074e6ada63fb483be5bb1e9233a68
Karollainny Teles <karollainnyteles@hotmail.com>
2022-01-07 16:41:03 -0300

    Injetando dependência

--- src/Estudo.UI.AppModelo/Controllers/HomeController.cs
+++ src/Estudo.UI.AppModelo/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Estudo.UI.AppModelo.Data;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,8 +9,17 @@ namespace Estudo.UI.AppModelo.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IPedidoRepository _pedidoRepository;
+
+        public HomeController(IPedidoRepository pedidoRepository)
+        {
+            _pedidoRepository = pedidoRepository;
+        }
+
         public IActionResult Index()
         {
+            var pedido = _pedidoRepository.ObterPedido();
+
             return View();
         }
     }
